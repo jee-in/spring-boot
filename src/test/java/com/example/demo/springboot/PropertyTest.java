bcfabc6eac95cd1389b1ef8b976f4fa6c357f404
jeein <159971124+jee-in@users.noreply.github.com>
2025-05-23 13:17:29 +0900

    Add Loading Property Source test (#6)

---
+++ src/test/java/com/example/demo/springboot/PropertyTest.java
@@ -0,0 +1,48 @@
+package com.example.demo.springboot;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.MutablePropertySources;
+import org.springframework.core.env.PropertySource;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+public class PropertyTest {
+
+    @Autowired
+    ConfigurableEnvironment environment;
+
+    @Value("${test.property}")
+    String testProperty;
+
+    @Test
+    void printAllPropertySources() {
+        MutablePropertySources propertySources = environment.getPropertySources();
+        propertySources.stream().forEach(source -> System.out.println(source.getName()));
+
+        assertThat(propertySources).anyMatch(propertySource -> propertySource.getName().equals("configurationProperties"));
+        assertThat(propertySources).anyMatch(propertySource -> propertySource.getName().equals("test"));
+        assertThat(propertySources).anyMatch(propertySource -> propertySource.getName().equals("Inlined Test Properties"));
+        assertThat(propertySources).anyMatch(propertySource -> propertySource.getName().equals("systemProperties"));
+        assertThat(propertySources).anyMatch(propertySource -> propertySource.getName().equals("systemEnvironment"));
+        assertThat(propertySources).anyMatch(propertySource -> propertySource.getName().equals("random"));
+        assertThat(propertySources).anyMatch(propertySource -> propertySource.getName().contains("application.properties"));
+        assertThat(propertySources).anyMatch(propertySource -> propertySource.getName().contains("application.yml"));
+        assertThat(propertySources).anyMatch(propertySource -> propertySource.getName().contains("applicationInfo"));
+
+        assertThat(testProperty)
+            .isEqualTo(
+                propertySources
+                    .stream()
+                    .filter(propertySource -> propertySource.getName().contains("application.properties"))
+                    .findFirst().get().getProperty("test.property")
+            );
+        System.out.println("Override testProperty value: " + testProperty);
+
+    }
+}
